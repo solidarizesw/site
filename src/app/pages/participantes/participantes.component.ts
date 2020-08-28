767da3b407064cb48582021ba5d05c0dd9974fd9
Juliana Câmara <julianabc@id.uff.br>
2020-08-28 15:06:34 -0300

    Create participantes.component.ts

---
+++ src/app/pages/participantes/participantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participantes',
+  templateUrl: './participantes.component.html',
+  styleUrls: ['./participantes.component.css']
+})
+export class ParticipantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
