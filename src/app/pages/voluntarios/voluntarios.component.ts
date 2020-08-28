3953f9139bccbccd4c8de1e6a0208f4157c6cdde
Juliana Câmara <julianabc@id.uff.br>
2020-08-28 15:09:27 -0300

    Create voluntarios.component.ts

---
+++ src/app/pages/voluntarios/voluntarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voluntarios',
+  templateUrl: './voluntarios.component.html',
+  styleUrls: ['./voluntarios.component.css']
+})
+export class VoluntariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
