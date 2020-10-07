a44b690b925f8049f79a3a54ea57c07ff1687e5b
Juliana Câmara <julianabc@id.uff.br>
2020-10-07 19:02:16 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+// a partir daqui
+import { HeaderComponent } from './pages/header/header.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ParticipantesComponent } from './pages/participantes/participantes.component';
+import { ServicosComponent } from './pages/servicos/servicos.component';
+import { VoluntariosComponent } from './pages/voluntarios/voluntarios.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
 
+
 const routes: Routes = [
+  {path : '', component: HeaderComponent},
+
+  {path : 'sobre', component: SobreComponent },
+ 
+  {path : 'participantes', component : ParticipantesComponent},
+  
+  {path : 'servicos', component: ServicosComponent},
+  
+  {path : 'voluntarios', component : VoluntariosComponent},
+  
+  {path : 'cadastro_proj', component : CadastroComponent},
+  
+  {path : 'contato', component: CadastroComponent},
+  
   {path :'login', component: LoginComponent},
 ];
 
