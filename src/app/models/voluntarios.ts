aed0705c28c30354cc181e6a9cf8e2414517bfb9
julianabc <julianabc@id.uff.br>
2020-11-27 16:37:17 -0300

    Create voluntarios.ts

---
+++ src/app/models/voluntarios.ts
@@ -0,0 +1,10 @@
+export interface Voluntarios {
+  id: number;
+  nome: string;
+  email: string;
+  curso: string;
+  motivacao: string;
+  status: string;
+
+  // não usar para pegar o arquivo por enquanto. apenas testar as validações
+}
