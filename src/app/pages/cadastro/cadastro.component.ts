1baa89415ed75205457c76575c39149f15928fc9
julianabc <julianabc@id.uff.br>
2020-12-08 17:57:30 -0300

    Update cadastro.component.ts

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Projetos } from './../../models/projetos';
+import { ProjetosService } from './../../services/projetos.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,10 +9,39 @@ import { Projetos } from './../../models/projetos';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
- // proj = {} as
-  constructor() { }
 
-  ngOnInit(): void {
+  proj = {} as Projetos;
+  projs: Projetos[];
+
+  constructor(private projServ: ProjetosService) {}
+
+ ngOnInit(){
+   this.getProj();
   }
 
-}
+  // apenas salvar o projeto
+  salvarProjeto(form: NgForm){
+    if(this.proj.id == undefined){
+      this.projServ.saveProj(this.proj).subscribe(() => {
+        this.cleanForm(form);
+      });
+    }
+  }
+
+  // mostra todos os projetos registrados
+  getProj(){
+    this.projServ.getProj().subscribe((projs: Projetos[]) => {
+      this.projs = projs;
+    });
+  } // fim do getProj
+
+
+  // limpar o formulario
+  cleanForm(form: NgForm){
+    this.getProj();
+    form.resetForm();
+    this.proj = {} as Projetos;
+  }
+
+
+} // fim da classe
