7259ccfee18ee4318a47892b4bcc04cb97b4f703
julianabc <julianabc@id.uff.br>
2020-12-08 11:00:18 -0300

    Update cadastro.component.ts

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Projetos } from './../../models/projetos';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
-
+ // proj = {} as
   constructor() { }
 
   ngOnInit(): void {
