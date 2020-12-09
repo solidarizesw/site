b6020c2371de4eb13f7cc5438f74e93f5646e3bb
julianabc <julianabc@id.uff.br>
2020-12-09 01:21:55 -0300

    Create volunts.component.ts

---
+++ src/app/pages/admin/tabelas/volunts/volunts/volunts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunts',
+  templateUrl: './volunts.component.html',
+  styleUrls: ['./volunts.component.scss']
+})
+export class VoluntsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
