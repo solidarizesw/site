e43d8fac3d2a474921458a975077a781f71974b6
julianabc <julianabc@id.uff.br>
2020-12-09 13:29:18 -0300

    Update admin.component.ts

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Voluntarios } from './../../models/voluntarios';
-import { VoluntariosService } from './../../services/voluntarios.service';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -11,52 +8,8 @@ import { NgForm } from '@angular/forms';
 
 export class AdminComponent implements OnInit {
 
-  volunt = {} as Voluntarios;
-  volunts: Voluntarios[];
-
-  constructor(private voluntServ: VoluntariosService) {}
-
   ngOnInit(){
-    this.getVolunt();
+
   }
 
-  saveVolunt(form: NgForm){
-    if(this.volunt.id !== undefined){
-      this.voluntServ.updateVolunt(this.volunt).subscribe(() => {
-        this.cleanForm(form);
-      });
-    }else{
-      this.voluntServ.saveVolunt(this.volunt).subscribe(() => {
-        this.cleanForm(form);
-      });
-    }
-  } // fim do saveVolunt
-
-  // mostra todos os voluntarios registrados
-  getVolunt(){
-    this.voluntServ.getVolunt().subscribe((volunts: Voluntarios[]) => {
-      this.volunts = volunts;
-    });
-  } // fim do getVolunt
-
-  // servico para apagar um dos voluntarios
-deleteVolunt(volunt: Voluntarios){
-  this.voluntServ.deleteVolunt(volunt).subscribe(() =>{
-    this.getVolunt();
-  });
-} // fim do deleteVolunt
-
-// edição de algum dado fornecido pelo voluntario
-editVolunt(volunt: Voluntarios){
-  this.volunt = { ...volunt };
-} // fim do editVolunt
-
-// limpar o formulario
-cleanForm(form: NgForm){
-  this.getVolunt();
-  form.resetForm();
-  this.volunt = {} as Voluntarios;
 }
-
-} // fim da classe
-
