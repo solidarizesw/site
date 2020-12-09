c104d3c9737b9b7a57ca458655b28ae926fcf06a
julianabc <julianabc@id.uff.br>
2020-12-09 01:21:40 -0300

    Create projs.component.ts

---
+++ src/app/pages/admin/tabelas/projs/projs/projs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projs',
+  templateUrl: './projs.component.html',
+  styleUrls: ['./projs.component.scss']
+})
+export class ProjsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
