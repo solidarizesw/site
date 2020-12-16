c0748147da287bd08b2e22ae2e481121efcf5c6d
julianabc <julianabc@id.uff.br>
2020-12-16 01:21:24 -0300

    Create table-volunt.component.ts

---
+++ src/app/pages/table-volunt/table-volunt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-volunt',
+  templateUrl: './table-volunt.component.html',
+  styleUrls: ['./table-volunt.component.scss']
+})
+export class TableVoluntComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
