529f9e50ca905b087820e511f78542a5f7a6f450
julianabc <julianabc@id.uff.br>
2020-12-16 02:30:11 -0300

    Update table-volunt.component.ts

--- src/app/pages/table-volunt/table-volunt.component.ts
+++ src/app/pages/table-volunt/table-volunt.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { VoluntariosService } from './../../services/voluntarios.service';
+import { Voluntarios } from './../../models/voluntarios';
+
 
 @Component({
   selector: 'app-table-volunt',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableVoluntComponent implements OnInit {
 
-  constructor() { }
+  volunt = {} as Voluntarios;
+  volunts: Voluntarios[];
+
+  constructor(private voluntServ: VoluntariosService) { }
 
   ngOnInit() {
+    this.getVolunt();
   }
 
-}
+  saveVolunt(form: NgForm){
+    if(this.volunt.id !== undefined){
+      this.voluntServ.updateVolunt(this.volunt).subscribe(() => {
+        this.cleanForm(form);
+      });
+    }else{
+      this.voluntServ.saveVolunt(this.volunt).subscribe(() => {
+        this.cleanForm(form);
+      });
+    }
+  } // fim do saveVolunt
+
+  // mostra todos os voluntarios registrados
+  getVolunt(){
+    this.voluntServ.getVolunt().subscribe((volunts: Voluntarios[]) => {
+      this.volunts = volunts;
+    });
+  } // fim do getVolunt
+
+  // servico para apagar um dos voluntarios
+  deleteVolunt(volunt: Voluntarios){
+    this.voluntServ.deleteVolunt(volunt).subscribe(() =>{
+    this.getVolunt();
+   });
+  } // fim do deleteVolunt
+
+  // edição de algum dado fornecido pelo voluntario
+  editVolunt(volunt: Voluntarios){
+    this.volunt = { ...volunt };
+  } // fim do editVolunt
+
+  // limpar o formulario
+  cleanForm(form: NgForm){
+    this.getVolunt();
+    form.resetForm();
+    this.volunt = {} as Voluntarios;
+  }
+
+} // fim da classe
